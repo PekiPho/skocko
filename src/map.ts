6549826326f97a52bea2dc37d910ac260efe26bc
PekiPho <v.a.r.d.games123@gmail.com>
2024-08-29 15:17:12 +0200

    added pictures

--- src/map.ts
+++ src/map.ts
@@ -38,6 +38,30 @@ export function generateMap(container){
         container.appendChild(red);
     }
 
+    let znakovi=document.createElement('div');
+    znakovi.classList.add("znakovi");
+
+    for(let i=0;i<6;i++){
+        let znak=document.createElement('div');
+        znak.classList.add('znak');
+
+        let image=document.createElement('img');
+
+        switch(i){
+            case 0: image.src='../assets/skocko.png'; break;
+            case 1: image.src='../assets/tref.png'; break;
+            case 2: image.src='../assets/pik.png'; break;
+            case 3: image.src='../assets/herc.png'; break;
+            case 4: image.src='../assets/karo.png'; break;
+            case 5: image.src='../assets/zvezda.png'; break;
+        }
+
+        znak.appendChild(image);
+        znakovi.appendChild(znak);
+    }
+
+    container.appendChild(znakovi);
+
     let btnContainer=document.createElement('div');
     btnContainer.classList.add('btnContainer');
 
